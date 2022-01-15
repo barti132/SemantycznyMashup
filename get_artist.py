098a483c8a130f3456321c9bc4a452698e8710f0
barti132 <barteksredzinski@gmail.com>
2022-01-15 22:33:15 +0100

    refactor

--- get_artist.py
+++ get_artist.py
@@ -6,6 +6,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 # cid & secret here
+cid = ""
+secret = ""
+
 
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
